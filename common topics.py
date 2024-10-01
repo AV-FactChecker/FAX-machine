8bd8039148d90de1d964438f1f78fc30dd5485ef
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2024-10-01 12:13:04 -0700

    Update common topics.py

--- common topics.py
+++ common topics.py
@@ -21,11 +21,11 @@ def generate_dense(sentences):
     return embeddings
 
 
-pc = Pinecone(api_key="${{secrets.PINECONE_API_KEY}}")
+pc = Pinecone(api_key="553e01e3-7e1a-46f3-ba75-098d1d9288df")
 
-if "factcheck-article-data-tvisha-avhs" not in pc.list_indexes().names():
+if "vp-factcheck-article-data-tvisha-avhs" not in pc.list_indexes().names():
     pc.create_index(
-        name="factcheck-article-data-tvisha-avhs",
+        name="vp-factcheck-article-data-tvisha-avhs",
         dimension=768, 
         metric="cosine",  
         spec=ServerlessSpec(
@@ -35,23 +35,23 @@ if "factcheck-article-data-tvisha-avhs" not in pc.list_indexes().names():
     )
 
 
-index = pc.Index("factcheck-article-data-tvisha-avhs")
+index = pc.Index("vp-factcheck-article-data-tvisha-avhs")
 
 
-topics = ["abortion", "environment", "economic policy", "foreign policy", "immigration", "war in Gaza", "police and safety", "voting rights", "education", "project 2025", "Kamala Harris", "Donald Trump", "Joe Biden", "Tim Walz", "JD Vance", "LGBTQ rights", "womens rights", "American politics", "USA", "presidential election", "presidential debate", "US debt", "race", "healthcare", "gun laws", "policy making", "US Economy", "RNC", "assassination", "arlington", "shooting", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming", "gay, transgender", "Trump assassination"]
+topics = ["presidential debate", "vice presidential debate", "abortion", "environment", "economic policy", "foreign policy", "immigration", "war in Gaza", "police and safety", "voting rights", "education", "project 2025", "Kamala Harris", "Donald Trump", "Joe Biden", "Tim Walz", "JD Vance", "LGBTQ rights", "womens rights", "American politics", "USA", "presidential election", "presidential debate", "US debt", "race", "healthcare", "gun laws", "policy making", "US Economy", "RNC", "assassination", "arlington", "shooting", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming", "gay, transgender", "Trump assassination"]
 
 
 count = 0
 
-for topic in topics:
-    newsapi = NewsApiClient(api_key='${{secrets.NEWS_API_KEY}}')
+for topic in topics[88:]:
+    newsapi = NewsApiClient(api_key='4cfdc3ef7a0340c3b1cff4cb672709fe')
 
     # from (datetime.date.today().replace(day=1)-datetime.timedelta(days=1)).replace(day=datetime.date.today().day).strftime("%Y %m %d")
     # to datetime.date.today().strftime("%Y %m %d")
     all_articles = newsapi.get_everything(
                                         q=topic,
-                                        from_param='2024-09-06',
-                                        to='2024-09-10',
+                                        from_param='2024-09-30',
+                                        to='2024-09-01',
                                         language='en',
                                         sort_by='relevancy',
                                         )
@@ -75,7 +75,7 @@ for topic in topics:
     for embedding in range(len(embeddings)):
         vectors.append(
             {
-                "id" : "article_" + str(9115+embedding + 100*count),
+                "id" : "article_" + str(4400000+embedding + 100*count),
                 "values" : dense_embeddings[embedding],
                 "sparse_values" : embeddings[embedding],
                 "metadata" : {"text": article_content[embedding]}
